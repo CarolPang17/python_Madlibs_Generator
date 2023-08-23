cabf1d6d507a449c3e2b7eab916e56c87fc49547
carolpang <carolpang17@gmail.com>
2023-08-22 23:28:13 -0700

    adding readme and founding the bracket location

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # python_Madlibs_Generator
 Python project , lesson by https://www.youtube.com/watch?v=21FnnGKSRZo&amp;t=1425s
+
+
+This project is building :
+
+1. find the presence of this angle bracket < > from story.txt
+2. in the angle breacker < >, get the every individual word
+3. take that word, store that in some kind of list that we're going to have access to
+4. replace it the word in the list with whatever the user told us 
---
+++ project2.py
@@ -0,0 +1,24 @@
+#get all texts from the story.txt
+with open("story.txt", "r") as f:
+    story = f.read()
+
+#create container to store the words
+words =[]
+#set the "start_of_word" initially to -1, so we can check if this stay -1, means did not move, did not found the location of bracket
+start_of_word = -1
+
+#targeting the words location
+target_start = "<"
+target_end = ">"
+
+#loop the angle bracket < >  
+for i, char in enumerate(story):
+    #if found the opening of the bracket "<", means that found the beinning of the word
+    if char == target_start:
+        #now change the value of "start_of_word "
+        start_of_word = i
+
+    # "start_of_word" not equal to -1 means the beginning of the bracket found
+    if char == target_end and start_of_word != -1:
+        # then we can take the entire word, and add that to the words list
+        word = story[start_of_word: i + 1 ]
